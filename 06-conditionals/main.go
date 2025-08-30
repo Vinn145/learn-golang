8a63f01c2020b156fd7ccb694058ea62dcfa6094
Kevin Andrea <zonaceluler6@gmail.com>
2025-08-30 11:15:34 +0700

    conditionals examples (if, else, switch)
    add switch statement for location
    update main.go with if-else and switch

--- 06-conditionals/main.go
+++ 06-conditionals/main.go
@@ -3,34 +3,56 @@ package main
 import "fmt"
 
 func main() {
+	// data pribadi
+	name := "Kevin Andrea"
 	age := 19
-
-	// if else if contoh umur
-	if age < 13 {
-		fmt.Println("Anak-anak")
-	} else if age < 20 {
-		fmt.Println("Remaja")
-	} else {
-		fmt.Println("Dewasa")
-	}
-
-	// switch dengan data lokasi nyata
+	graduated := true
+	school := "SMK 1 Sumber"
 	district := "Batangan"
 	village := "Kuniran"
 
+	fmt.Println("=== Profil Singkat ===")
+	fmt.Println("Nama:", name)
+	fmt.Println("Umur:", age)
+	fmt.Println("Sekolah:", school)
+	fmt.Println("Sudah lulus:", graduated)
+
+	// contoh IF
+	if age < 13 {
+		fmt.Println("Status umur: Anak-anak")
+	} else if age < 20 {
+		fmt.Println("Status umur: Remaja")
+	} else {
+		fmt.Println("Status umur: Dewasa")
+	}
+
+	// contoh SWITCH untuk lokasi
 	switch district {
 	case "Pati":
-		fmt.Println("Kamu di Kabupaten Pati")
+		fmt.Println("Lokasi: Kabupaten Pati")
 	case "Batangan":
-		fmt.Println("Kamu di Kecamatan Batangan")
+		fmt.Println("Lokasi: Kecamatan Batangan")
 	default:
-		fmt.Println("Kecamatan tidak dikenal")
+		fmt.Println("Lokasi: Kecamatan lain")
 	}
 
 	switch village {
 	case "Kuniran":
-		fmt.Println("Kamu tinggal di Desa Kuniran")
+		fmt.Println("Tinggal di Desa Kuniran")
 	default:
 		fmt.Println("Desa tidak dikenal")
+	}
+
+	// contoh gabungan IF + SWITCH
+	if graduated {
+		fmt.Println("Status: Baru lulus,", school)
+		switch age {
+		case 18, 19, 20:
+			fmt.Println("saya siap lanjut kuliah atau kerja 👍")
+		default:
+			fmt.Println("Umur tidak biasa untuk baru lulus SMK")
+		}
+	} else {
+		fmt.Println("Masih sekolah di", school)
 	}
 }
